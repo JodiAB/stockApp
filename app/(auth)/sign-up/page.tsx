69b3aa0d3e4c6f2308126d77d51dcaf00b592625
JodiAB <jodiabrahams9@gmail.com>
2025-10-13 18:04:58 +0200

    auth complete

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,14 +1,16 @@
-'use client'
-import React from 'react'
+'use client';
+
 import {useForm} from "react-hook-form";
-import {useRouter} from "next/navigation";
 import {Button} from "@/components/ui/button";
 import InputField from "@/components/forms/InputField";
-// import {CountrySelectField} from "@/components/forms/CountrySelectField";
 import SelectField from "@/components/forms/SelectField";
 import {INVESTMENT_GOALS, PREFERRED_INDUSTRIES, RISK_TOLERANCE_OPTIONS} from "@/lib/constants";
-import FooterLink from "@/components/forms/FooterLink";
 import {CountrySelectField} from "@/components/forms/CountrySelectField";
+import FooterLink from "@/components/forms/FooterLink";
+import {signUpWithEmail} from "@/lib/actions/auth.actions";
+import {useRouter} from "next/navigation";
+import {toast} from "sonner";
+
 
 const SignUp = () => {
     const router = useRouter()
@@ -31,26 +33,17 @@ const SignUp = () => {
     }, );
 
     const onSubmit = async (data: SignUpFormData) => {
-        try{
-            console.log(data)
-        }catch (e) {
-            console.error(e)
+        try {
+            const result = await signUpWithEmail(data);
+            if(result.success) router.push('/');
+        } catch (e) {
+            console.error(e);
+            toast.error('Sign up failed', {
+                description: e instanceof Error ? e.message : 'Failed to create an account.'
+            })
         }
-
     }
 
-
-    // const onSubmit = async (data: SignUpFormData) => {
-    //     try {
-    //         const result = await signUpWithEmail(data);
-    //         if(result.success) router.push('/');
-    //     } catch (e) {
-    //         console.error(e);
-    //         toast.error('Sign up failed', {
-    //             description: e instanceof Error ? e.message : 'Failed to create an account.'
-    //         })
-    //     }
-    // }
     return (
         <>
             <h1 className="form-title">Sign Up & Personalize</h1>
@@ -68,7 +61,7 @@ const SignUp = () => {
                 <InputField
                     name="email"
                     label="Email"
-                    placeholder="contact@gmail.com"
+                    placeholder="contact@jsmastery.com"
                     register={register}
                     error={errors.email}
                     validation={{ required: 'Email name is required', pattern: /^\w+@\w+\.\w+$/, message: 'Email address is required' }}
